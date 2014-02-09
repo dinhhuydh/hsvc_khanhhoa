f798a8c36409a2d084a20a162aafde2c9e48b1ee
Huy Dinh <dinhhuydh@gmail.com>
2014-02-09 14:15:25 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SvcKhanhhoa::Application.config.secret_token = '66e1b36a1e5466153d777d165ef006fea7c1d979f251ec380d498fcbf8120bd7d5dcdef3fe560c1ad730fd79dd2c786e89b0ce60c14b7d2b0886bd296da41ad7'
