7bd79e2d3415968e83d3a31dd2451df061cc1eca
Huy Dinh <dinhhuydh@gmail.com>
2014-03-02 15:27:28 +0700

    add image slider to home page

---
+++ app/assets/javascripts/homepage.js.coffee
@@ -0,0 +1,5 @@
+$ ->
+  $('#slider').nivoSlider({
+    prevText: 'Trước',
+    nextText: 'Sau'
+  })
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def homepage_images
+    home_page = Refinery::Page.find_by_link_url('/')
+    home_page.images
+  end
 end
